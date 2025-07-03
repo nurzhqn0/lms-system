200a908f99b951f828b354ebfbadaf64454d0fd5
Nurzhan <nurzhqn@gmail.com>
2025-07-03 18:28:32 +0500

    بسم الله الرحمن الرحيم

---
+++ src/main/java/kz/bitlab/LMS/System/LmsSystemApplication.java
@@ -0,0 +1,13 @@
+package kz.bitlab.LMS.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/bitlab/LMS/System/LmsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.bitlab.LMS.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
